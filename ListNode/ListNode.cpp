62e6b48fbc461d97fab11359cfcc5915213505c2
yr0418 <1933352594@qq.com>
2021-02-24 21:16:23 +0800

    简单算法2：反转单链表

--- ListNode/ListNode.cpp
+++ ListNode/ListNode.cpp
@@ -12,7 +12,7 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 
-//尾插法创建带表头节点单链表，先进先出
+//尾插法创建不带表头节点单链表，先进先出
 int initNodeList_01(ListNode *&L, int a[], int length){
     ListNode *s, *r;
     L = (ListNode *)malloc(sizeof(ListNode));
@@ -24,13 +24,15 @@ int initNodeList_01(ListNode *&L, int a[], int length){
         r->next = s; s->next = NULL;
         r = s;
     }
+    //头结点后移一位，使得不带头结点
+    L = L->next;
     return OK;
 }
 
 //顺序打印链表
 void printNodeList(ListNode *L){
     ListNode *p;
-    p = L->next;
+    p = L;
     while (p){
         printf("%d  ", p->val);
         p = p->next;
@@ -40,6 +42,7 @@ void printNodeList(ListNode *L){
 
 // 移除链表中的指定元素
 ListNode* removeElements(ListNode *head, int val) {
+    //哨兵节点，类似头结点
     ListNode* sentinel = new ListNode(0);
     sentinel->next = head;
 
@@ -63,12 +66,34 @@ ListNode* removeElements(ListNode *head, int val) {
     return ret;
 }
 
+//反转单链表
+ListNode* reverseList(ListNode* head) {
+    //哨兵节点，类似头结点
+    ListNode* sentinel = new ListNode(0);
+    sentinel->next = head;
+
+    ListNode *right, *curr;
+    curr = sentinel->next;
+    sentinel->next = nullptr;
+    while (curr != nullptr){
+        right = curr->next;
+        curr->next = sentinel->next;
+        sentinel->next = curr;
+        curr = right;
+    }
+    head = sentinel->next;
+    delete sentinel;
+    return head;
+}
+
+
 int main(){
     int a[] = {1,2,6,3,4,5,6};
     int b = 0;
     ListNode *L;
     initNodeList_01(L, a, 7);
-    removeElements(L, 6);
+    printNodeList(L);
+    reverseList(L);
     printNodeList(L);
     return 0;
 }
