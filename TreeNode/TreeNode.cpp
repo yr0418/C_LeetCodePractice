845c05ba4d08dde2da17ee9bb90d1ed8882332e7
yr0418 <1933352594@qq.com>
2021-02-26 17:04:21 +0800

    简单算法7：二叉搜索树的最近公共祖先

--- TreeNode/TreeNode.cpp
+++ TreeNode/TreeNode.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<malloc.h>
+#include<vector>
 using namespace std;
 #define OK 1
 #define ERROR 0
@@ -40,4 +41,60 @@ TreeNode* invertTree(TreeNode* root) {
     root->left = right;
     root->right = left;
     return root;
+}
+
+/**
+ * 二叉搜索树的最近公共祖先
+ * 解法一：两次遍历
+ */ 
+TreeNode* lowestCommonAncestor_1(TreeNode* root, TreeNode* p, TreeNode* q) {
+    vector<TreeNode*> path_p, path_q;
+    TreeNode* curr;
+    path_p = getPath(root, p);
+    path_q = getPath(root, q);
+    for(int i = 0; i < path_p.size() && i < path_q.size(); ++i){
+        if(path_p[i]->val == path_q[i]->val){
+            curr = path_p[i-1];
+        }
+        else{
+            break;
+        }
+    }
+    return curr;
+}
+
+vector<TreeNode*> getPath(TreeNode* root, TreeNode* target){
+    vector<TreeNode*> path;
+    TreeNode* curr = root;
+    while(curr->val != target->val){
+        path.push_back(curr);
+        if(curr->val < target->val){
+            curr = curr->right;
+        }
+        else{
+            curr = curr->left;
+        }
+    }
+    path.push_back(curr);
+    return path;
+}
+
+/**
+ * 二叉搜索树的最近公共祖先
+ * 解法二：一次遍历 
+ */
+TreeNode* lowestCommonAncestor_2(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* ancestor = root;
+    while (true) {
+        if (p->val < ancestor->val && q->val < ancestor->val) {
+            ancestor = ancestor->left;
+        }
+        else if (p->val > ancestor->val && q->val > ancestor->val) {
+            ancestor = ancestor->right;
+        }
+        else {
+            break;
+        }
+    }
+    return ancestor;
 }
